420578769a2cdc841ada30d778836e88c4021889
ilucha101 <41263527+ilucha101@users.noreply.github.com>
2026-07-10 14:16:20 +0200

    Update config.h

--- keyboards/keebox/yr80_r2/keymaps/vial/config.h
+++ keyboards/keebox/yr80_r2/keymaps/vial/config.h
@@ -24,15 +24,15 @@
 // - static light
 // - static gradient
 // - rainbow swirl (animated gradient moving across the LED order)
-// Removed to save flash: breathing, rainbow mood, snake, knight, christmas, etc.
+// Removed to save flash: breathing, snake, knight, christmas, etc.
 #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
 #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
 
 #undef RGBLIGHT_EFFECT_ALTERNATING
 #undef RGBLIGHT_EFFECT_BREATHING
 #undef RGBLIGHT_EFFECT_CHRISTMAS
 #undef RGBLIGHT_EFFECT_KNIGHT
-#undef RGBLIGHT_EFFECT_RAINBOW_MOOD
 #undef RGBLIGHT_EFFECT_RGB_TEST
 #undef RGBLIGHT_EFFECT_SNAKE
 #undef RGBLIGHT_EFFECT_TWINKLE
